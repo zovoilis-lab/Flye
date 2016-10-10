1d19fc73e057735afe78fa8fdad6144aabbfae65
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-10-10 13:08:58 +0200

    Some fixes in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='abruijn',
       version='0.4b',
@@ -7,6 +7,13 @@ setup(name='abruijn',
       author='Mikhail Kolmogorov',
       author_email = '',
       license='BSD-3-Clause',
+      packages=find_packages(),
+      entry_points={ 
+         'console_scripts': [
+              'abruijn = abruijn.main:main'
+          ]
+      },
+      # Could skip abruin.py in the entrypoint works
       scripts = ['abruijn.py', 'bin/abruijn-assemble', 'bin/abruijn-polish'],
       data_files=['resource/nano_homopolymers.mat', 'resource/nano_substitutions.mat', 'resource/p6c4_homopolymers.mat', 'resource/p6c4_substitutions.mat', 'resource/pacbio_homopolymers.mat', 'resource/pacbio_substitutions.mat']
       )
