115f255c7f8d3c7fe36c51d1a82c1e2e352381a4
Emil Hägglund <haegglund.emil@gmail.com>
2016-10-11 12:26:52 +0200

    removed print line from abruijn script

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup, find_packages
+from os import system
+
+system('make')
 
 setup(name='abruijn',
       version='0.4b',
@@ -8,13 +11,8 @@ setup(name='abruijn',
       author_email = '',
       license='BSD-3-Clause',
       packages=find_packages(),
-      entry_points={ 
-         'console_scripts': [
-              'abruijn = abruijn.main:main'
-          ]
-      },
-      # Could skip abruin.py in the entrypoint works
-      scripts = ['abruijn.py', 'bin/abruijn-assemble', 'bin/abruijn-polish'],
-      data_files=['resource/nano_homopolymers.mat', 'resource/nano_substitutions.mat', 'resource/p6c4_homopolymers.mat', 'resource/p6c4_substitutions.mat', 'resource/pacbio_homopolymers.mat', 'resource/pacbio_substitutions.mat']
+      scripts = ['bin/abruijn-assemble', 'bin/abruijn-polish', 'scripts/abruijn'],
+      data_files=['resource/nano_homopolymers.mat', 'resource/nano_substitutions.mat', 'resource/p6c4_homopolymers.mat',
+                  'resource/p6c4_substitutions.mat', 'resource/pacbio_homopolymers.mat', 'resource/pacbio_substitutions.mat']
       )
 
