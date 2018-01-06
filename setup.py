070f901b220aa65dabb79d04326548fbd697e100
Mikhail Kolmogorov <fenderglass@gmail.com>
2018-01-06 09:13:24 -1000

    fixed installation script + python version check

--- setup.py
+++ setup.py
@@ -2,18 +2,28 @@
 #This file is a part of ABruijn program.
 #Released under the BSD license (see LICENSE file)
 
+from __future__ import print_function
+import sys
+
+#Check Python version
+if sys.version_info[:2] != (2, 7):
+    print("Error: Flye requires Python version 2.7 ({0}.{1} detected)."
+          .format(sys.version_info[0], sys.version_info[1]))
+    sys.exit(-1)
+
 from distutils.core import setup
 from distutils.command.build import build as DistutilsBuild
 import subprocess
 
 from flye.__version__ import __version__
 
+
 class MakeBuild(DistutilsBuild):
     def run(self):
         try:
             subprocess.check_call(['make'])
         except subprocess.CalledProcessError as e:
-            print "Compilation error: ", e
+            print ("Compilation error: ", e)
             return
         DistutilsBuild.run(self)
 
@@ -25,7 +35,7 @@ setup(name='flye',
       author_email = 'fenderglass@gmail.com',
       license='BSD-3-Clause',
       packages=['flye'],
-      package_data={'flye': ['resource/*.mat']},
+      package_data={'flye': ['resource/*']},
       scripts = ['bin/flye-assemble', 'bin/flye-polish',
                  'bin/flye-repeat', 'bin/flye', 'bin/flye-minimap2'],
       cmdclass={'build': MakeBuild}
