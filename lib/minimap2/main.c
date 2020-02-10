da64ddf12f30ec9d1cf1053dc10507391c966f06
Mikhail Kolmogorov <fenderglass@gmail.com>
2020-02-10 14:24:09 -0800

    custom minimap2 option to add SEQ for secondary alignments

--- lib/minimap2/main.c
+++ lib/minimap2/main.c
@@ -66,6 +66,7 @@ static ko_longopt_t long_options[] = {
 	{ "junc-bed",       ko_required_argument, 340 },
 	{ "junc-bonus",     ko_required_argument, 341 },
 	{ "sam-hit-only",   ko_no_argument,       342 },
+	{ "secondary-seq",  ko_no_argument,       343 },
 	{ "help",           ko_no_argument,       'h' },
 	{ "max-intron-len", ko_required_argument, 'G' },
 	{ "version",        ko_no_argument,       'V' },
@@ -209,6 +210,7 @@ int main(int argc, char *argv[])
 		else if (c == 338) opt.max_qlen = mm_parse_num(o.arg); // --max-qlen
 		else if (c == 340) junc_bed = o.arg; // --junc-bed
 		else if (c == 342) opt.flag |= MM_F_SAM_HIT_ONLY; // --sam-hit-only
+		else if (c == 343) opt.flag |= MM_F_SECONDARY_SEQ; // --secondary-seq
 		else if (c == 314) { // --frag
 			yes_or_no(&opt, MM_F_FRAG_MODE, o.longidx, o.arg, 1);
 		} else if (c == 315) { // --secondary
